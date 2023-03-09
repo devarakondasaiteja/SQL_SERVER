ecd2066c196ddf56da4fe49b8a176cb5514cb94e
Devarakonda Sai teja <89306988+devarakondasaiteja@users.noreply.github.com>
2023-03-09 11:33:08 +0530

    Add files via upload

---
+++ stat.sql
@@ -0,0 +1,34 @@
+﻿
+--STATISTICS
+
+/*Binomial Distribution
+
+    Fixed no. of Trials (n) [10 pie throws].
+    Although, only 2 possible outcomes are possible.
+    Basically, a probability of Success is constant(p).
+    Moreover, each Trial is independent.
+    Also, it predicts no.s of successes within a set no. of trials.
+    Generally, we use it to test for Independence.
+
+Poisson Distribution
+
+    Infinite no. of Trials.
+    Also, it has unlimited no. of outcomes possible.
+    Basically, mean of the distribution is the same for all intervals.
+    Generally, no. of occurrence in any given interval independent of others.
+    Also, it Predicts no. of occurrences per unit, Time, Space.
+    Moreover, we use it to test for independence.*/
+ -- OLS IN STATISTICS
+ /*Basically, it is a type of statistical technique. That is being used for modeling. Also, 
+ used for analysis of linear relationships between a response variable. Further, if there is a relationship between two variables appears to be linear.
+ Then a straight line can be fit to the data to model the relationship.
+The linear equation for a bivariate regression takes the following form:
+y=mx+c
+Where y = response(dependent) variable
+m = gradient(slope)
+x = predictor(independent) variable
+C = is the intercept*/
+
+-- chi squre test
+--Basically, it is a statistical method which is being used to determine if two categorical variables have a significant correlation between them.
+--Also, we have to choose both variables from the same population And they should be categorized as − Male/Female, Red/Green Yes/No, etc.
