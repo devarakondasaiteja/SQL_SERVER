9d17a631056fadb84a3f1971c1d4d99dac6fffa7
Devarakonda Sai teja <89306988+devarakondasaiteja@users.noreply.github.com>
2023-03-09 11:28:32 +0530

    Add files via upload

---
+++ SQL-nt.sql
@@ -0,0 +1,11 @@
+--SQL
+/*Data Science is the study and analysis of data. In order to analyze the data, we need to extract it from the database. 
+This is where SQL comes. Relational Database Management is an important part of Data Science*/
+
+/*Many database platforms are modelled after SQL. This is because it has become a standard for many database systems.
+modern big data systems like Hadoop, Spark make use of SQL for maintaining relational database systems and processing structured data. */
+
+/*  Data Science needs SQL in order to handle structured data. This structured data is stored in relational databases. 
+Therefore, in order to query these databases, must have a knowledge of SQL.
+Big Data Platforms like Hadoop provides an extension for querying SQL commands for manipulating data through HiveQL.*/
+
